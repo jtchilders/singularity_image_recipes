66f208327238b6d4d4fefbdb9769232113aa1aa8
Taylor Childers <jchilders@alcfwl054.alcf.anl.gov>
2019-07-31 10:32:26 -0500

    updating submit.sh to match website

--- submit.sh
+++ submit.sh
@@ -11,12 +11,10 @@ CONTAINER=$1
 module swap cray-mpich cray-mpich-abi
 
 # include CRAY_LD_LIBRARY_PATH in to the system library path
-export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
-# also need this additional library
-export LD_LIBRARY_PATH=/opt/cray/wlm_detect/1.3.2-6.0.6.0_3.8__g388ccd5.ari/lib64/:$LD_LIBRARY_PATH
 # in order to pass environment variables to a Singularity container create the variable
 # with the SINGULARITYENV_ prefix
-export SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+export SINGULARITYENV_LD_LIBRARY_PATH=/opt/cray/wlm_detect/default/lib64/:$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+
 # print to log file for debug
 echo $SINGULARITYENV_LD_LIBRARY_PATH
 
@@ -26,4 +24,4 @@ TOTAL_RANKS=$(( $COBALT_JOBSIZE * $RANKS_PER_NODE ))
 # this simply runs the command 'ldd /myapp/pi' inside the container and should show that
 # the app is running agains the host machines Cray libmpi.so not the one inside the container
 # run my contianer like an application, which will run '/myapp/pi'
-aprun -n $TOTAL_RANKS -N $RANKS_PER_NODE singularity run -B /opt:/opt:ro $CONTAINER
+aprun -n $TOTAL_RANKS -N $RANKS_PER_NODE singularity run -B /opt -B /etc/alternatives $CONTAINER
